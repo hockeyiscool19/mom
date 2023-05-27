fb71d7581196a04edba36d888a5705fc64878e21
hockeyiscool19 <jordaneisenman@gmail.com>
2023-05-27 15:57:39 -0400

    stuff

--- ECS/docker.sh
+++ ECS/docker.sh
@@ -1,11 +1,11 @@
 
 
-docker build --no-cache -t my-lambda .
-docker run -it my-lambda
+docker build --no-cache -t my-ecs .
+docker run -it my-ecs
 
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 363194772334.dkr.ecr.us-east-1.amazonaws.com
 docker push 363194772334.dkr.ecr.us-east-1.amazonaws.com/mom-texter:tag
-docker tag my-lambda:latest 363194772334.dkr.ecr.us-east-1.amazonaws.com/mom-texter:tag
+docker tag my-ecs:latest 363194772334.dkr.ecr.us-east-1.amazonaws.com/mom-texter:tag
 docker pull 363194772334.dkr.ecr.us-east-1.amazonaws.com/mom-texter:tag
-docker run -it --name temp6 363194772334.dkr.ecr.us-east-1.amazonaws.com/mom-texter:tag
+docker run -it --name temp7 363194772334.dkr.ecr.us-east-1.amazonaws.com/mom-texter:tag
 
--- cli/index.js
+++ cli/index.js
@@ -1,8 +1,8 @@
 
 const prompt = require('./src/services/prompt');
 const createEvent = require('./src/services/eventBridge');
-
 require('dotenv').config({ path: './dev.env.gitignore' });
+
 const SUBNET_ID = process.env.SUBNET_ID;
 console.log('SUBNET_ID:', SUBNET_ID);
 
@@ -13,12 +13,10 @@ async function main() {
         console.log(date);
     }
     );
-    // for date in tripDates, create event for that date 
     for (const date of tripDates) {
         const scheduledDate = new Date(date);
-        // Make the date be at 8:00PM
         scheduledDate.setHours(20);
-        scheduledDate.setMinutes(scheduledDate.getMinutes()); // Set the scheduled time to two minutes from the current date
+        scheduledDate.setMinutes(scheduledDate.getMinutes()); 
         console.log('Scheduled date:', scheduledDate);
         createEvent(scheduledDate);
     }
