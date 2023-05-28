90b65e1c2953671dde4a3c80a58f7acde55f08a3
hockeyiscool19 <65208198+hockeyiscool19@users.noreply.github.com>
2023-05-28 11:26:38 -0400

    Update docker.sh

--- cli/docker.sh
+++ cli/docker.sh
@@ -7,7 +7,7 @@ case "$1" in
   -b) docker build -t my-cli . ;;
   -r) docker run -it my-cli ;;
   -l) aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $AWS_ACCOUNT_NUM.dkr.ecr.us-east-1.amazonaws.com ;;
-  -p) docker push 363194772334.dkr.ecr.us-east-1.amazonaws.com/mom-texter:tag && docker tag my-cli:latest $AWS_ACCOUNT_NUM.dkr.ecr.us-east-1.amazonaws.com/mom-texter:tag ;;
+  -p) docker push $AWS_ACCOUNT_NUM.dkr.ecr.us-east-1.amazonaws.com/mom-texter:tag && docker tag my-cli:latest $AWS_ACCOUNT_NUM.dkr.ecr.us-east-1.amazonaws.com/mom-texter:tag ;;
   -m) docker tag my-cli:latest $AWS_ACCOUNT_NUM.dkr.ecr.us-east-1.amazonaws.com/mom-texter:tag ;;
   -r) docker stop temp && docker rm temp ;;
   -a) docker build --no-cache -t my-cli . && 
