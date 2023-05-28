74ed406ea8a69121b9f04d93aec1cb1ea02dcd1b
hockeyiscool19 <65208198+hockeyiscool19@users.noreply.github.com>
2023-05-28 11:03:50 -0400

    Update dock.sh

--- ECS/dock.sh
+++ ECS/dock.sh
@@ -20,16 +20,3 @@ case "$1" in
 
   *) echo "Invalid option";;
 esac
-
-
-
-
-# docker build --no-cache -t my-ecs .
-# docker run -it my-ecs
-
-# aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 363194772334.dkr.ecr.us-east-1.amazonaws.com
-# docker push 363194772334.dkr.ecr.us-east-1.amazonaws.com/mom-texter:tag
-# docker tag my-ecs:latest 363194772334.dkr.ecr.us-east-1.amazonaws.com/mom-texter:tag
-# docker pull 363194772334.dkr.ecr.us-east-1.amazonaws.com/mom-texter:tag
-# docker run -it --name temp7 363194772334.dkr.ecr.us-east-1.amazonaws.com/mom-texter:tag
-
